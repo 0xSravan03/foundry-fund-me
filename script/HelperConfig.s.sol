287a7072addea983865364b6052862b2f9b4e798
Sravan T P <sravantp03@gmail.com>
2023-06-13 17:19:03 +0530

    added Mock deployment for anvil chain

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 // Used for,
 // 1. Deploy Mock when we are on local anvil chain
@@ -14,11 +15,14 @@ contract HelperConfig is Script {
 
     NetworkConfig public activeNetworkConfig;
 
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
     constructor() {
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
         } else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
 
@@ -27,7 +31,20 @@ contract HelperConfig is Script {
         return sepoliaNetworkConfig;
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        // Deploy Mock contract
+        // and return the address
 
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        // This is because we actually want this deployment broadcast to anvil, other will it will only simulate
+        vm.startBroadcast();
+        MockV3Aggregator aggregatorV3 = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilNetworkConfig = NetworkConfig({ priceFeed : address(aggregatorV3)});
+        return anvilNetworkConfig;
     }
 }
